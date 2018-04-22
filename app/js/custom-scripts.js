4fbd5226dc170fe706a7cda5bd27db6314fc69c0
DafLec <dafnelecona@gmail.com>
2018-04-21 23:27:53 -0500

    Saving projects works and unnecessary files removed

--- app/js/custom-scripts.js
+++ app/js/custom-scripts.js
@@ -8,12 +8,6 @@ function clearStoreDB() {
 }
 // Fills table, info stored in store.js in valores.html
 function fillTable() {
-
-    // //Fills table with all projects.
-    // store.each(function(value, key) {
-    //     $("#projectTable").append("<tr><td>"+ store.get(key).name+"</td><td>Criteria 1</td><td>Criteria 2</td></tr>");
-    // });
-
     //Fills table with all projects.
     store.get('proyects').each(function(value, key) {
         var html;
@@ -29,48 +23,6 @@ function fillProjectTable() {
 
 }
 // Ends fill table, info stored in store.js-->
-
-// // Fills table, info stored in store.js in valores.html
-// function fillCriteriaTable() {
-//
-//     //Fills table with all projects.
-//     store.each(function(value, key) {
-//         var html = '';
-//         html = ;
-//         $("#projectTable").append(html);
-//     });
-// }
-// // Ends fill table, info stored in store.js-->
-
-// Creates project, stores it in store.js -->
-function createProject() {
-    //Gets information from forms
-    var projectName = document.getElementById("projectName").value;
-    var projectDescription = document.getElementById("projectDescription").value;
-    var projectQty = document.getElementById("projectQty").value;
-
-    if (projectName != '') {
-        //Sets values to the store object
-        //If there is no project with that name, it will be created, otherwise it will alert the user.
-        if (store.get(projectName) == null) {
-            store.set(projectName, {name: projectName, description: projectDescription, quantity: projectQty});//Stores information about project in storage
-            var project = store.get(projectName);
-            console.log("Project created! \nName: " + project.name + "\nDescription: " + project.description + "\nQuantity: " + project.quantity + "\n");
-
-        } else if (store.get(projectName) !== null) {
-            swal("Error!", "Lo sentimos el proyecto ya existe!", "error")        }
-    }else {
-        alert("Porfavor escriba un nombre para el proyecto");
-    }
-
-    //TODO:Delete after testing.
-    //Prints in console all projects.
-    store.each(function(value, key) {
-        console.log("Name: " + store.get(key).name);
-        console.log("Description: " + store.get(key).description);
-        console.log("Quantity: " + store.get(key).quantity + "\n");
-    })
-}
 
 function createHtmlSwal(html) {
     console.log(html);
@@ -95,42 +47,10 @@ function createHtmlSwal(html) {
 
 // Ends create project, stored in store.js-->
 $(document).ready(function () {
-
     //Needed for materialize css.
     $('.modal').modal();
     $('select').formSelect();
     $('.tabs').tabs({swipeable: true});
-
-    // $('#addProject').on('click', function () {
-    //     var html;
-    //     html = '' +
-    //         '<form onsubmit="createProject()">'+
-    //         '   <div class="form-group">' +
-    //         '       <label for="projectName">Nombre del proyecto</label>' +
-    //         '       <input id="projectName" type="text" class="form-control text-center" placeholder="Nombre">' +
-    //         '    </div>' +
-    //         '    <div class="form-group">' +
-    //         '       <label for="projectDescription">Descripción</label>' +
-    //         '       <textarea id="projectDescription" class="form-control"></textarea>' +
-    //         '    </div>' +
-    //         '    <div>' +
-    //         '       <label for="projectQty">Costo</label>' +
-    //         '       <input id="projectQty" type="number" class="form-control text-center" placeholder="Cantidad">' +
-    //         '    </div>' +
-    //         '    <br>' +
-    //         '    <div class="text-center">' +
-    //         '       <button type="submit" class="btn btn-outline-success">Guardar</button>' +
-    //         '    </div>' +
-    //         '</form>';
-    //     console.log("Project add", html);
-    //     createHtmlSwal(html);
-    // });
-
-    // $('#addProject').on('click', function () {
-    //     var html = '';
-    //     console.log("Project add");
-    //     createHtmlSwal(html);
-    // });
 
     // Creates new criteria
     $('#newCriteria-Form').submit(function (e) {
@@ -144,18 +64,36 @@ $(document).ready(function () {
             //Sets values to the store object
             //If there is no criteria with that name, it will be created, otherwise it will alert the user.
             if (store.get(data[0].value) == null) {
-                store.set(data[0].value, {name: data[0].value, quantity: data[1].value});//Stores information about project in storage
-
+                store.set(data[0].value, {name: data[0].value, quantity: data[1].value});//Stores information about criteria in storage
                 var criteria = store.get(data[0].value);
                 console.log("Criteria created! \nName: " + criteria.name + "\nQuantity: " + criteria.quantity + "\n");
-
                 $('#newCriteria-Form').trigger('reset');//Cleans/Restarts #newCriteria-Form.
 
             } else if (store.get(data[0].value) !== null) {
-                alert("Lo sentimos, este criterio ya existe!");
+                alert("Lo sentimos, este criterio ya existe");
             }
         }else {
             alert("Porfavor escriba un nombre para el criterio");
+        }
+    });
+    
+    //Creates new project
+    $('#newProject-Form').submit(function(e) {
+        e.preventDefault();
+        //Gets values from the form
+        var data = $(this).serializeArray();
+        console.log("Proyecto: ",data);
+        if(data[0].value != ''){
+            if(store.get(data[0].value) == null){
+                store.set(data[0].value, {name: data[0].value, description: data[1].value, cost: data[2].value});
+                var project = store.get(data[0].value);
+                console.log('Project created! \nName: '+project.name + '\nDescription: '+project.description+'\nQuantity: '+project.cost);
+                $('newProject-Form').trigger('reset');
+            }else if (store.get(data[0].vale) !== null){
+                alert("Lo sentimos, este proyecto ya existe");
+            }
+        }else{
+            alert("Por favor escriba el nombre del proyecto");
         }
     });
 });
